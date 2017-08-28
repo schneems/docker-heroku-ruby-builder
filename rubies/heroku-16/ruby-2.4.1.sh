211057f7ac0891f2f8b5f7e25606ea89a13593e4
schneems <richard.schneeman@gmail.com>
2017-08-28 16:17:36 -0500

    RUBYGEMS_VERSION=2.6.13 for Ruby 2.4.1

--- rubies/heroku-16/ruby-2.4.1.sh
+++ rubies/heroku-16/ruby-2.4.1.sh
@@ -2,4 +2,4 @@
 
 source `dirname $0`/../common.sh
 
-docker run -v $OUTPUT_DIR:/tmp/output -v $CACHE_DIR:/tmp/cache -e VERSION=2.4.1  -e STACK=heroku-16 hone/ruby-builder:heroku-16
+docker run -v $OUTPUT_DIR:/tmp/output -v $CACHE_DIR:/tmp/cache -e VERSION=2.4.1  -e STACK=heroku-16 -e RUBYGEMS_VERSION=2.6.13 hone/ruby-builder:heroku-16
